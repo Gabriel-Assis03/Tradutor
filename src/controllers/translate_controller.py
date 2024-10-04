c578773ac7e2919597e91657279f72bd5f31ba9a
Gabriel-Assis03 <gabrieleboltffggff@gmail.com>
2024-10-04 18:14:22 -0300

    req 5

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template
-# from deep_translator import GoogleTranslator
+from flask import Blueprint, render_template, request
+from deep_translator import GoogleTranslator
 from models.language_model import LanguageModel
 # from models.history_model import HistoryModel
 
@@ -12,15 +12,26 @@ translate_controller = Blueprint("translate_controller", __name__)
 def index():
     data = LanguageModel.find()
     formatLang = []
+    text_to_translate = (
+        request.form.get("text-to-translate")
+        or "O que deseja traduzir?"
+    )
+    translate_from = request.form.get("translate-from") or "pt"
+    translate_to = request.form.get("translate-to") or "en"
+    translated = GoogleTranslator(
+        source=translate_from, target=translate_to
+    ).translate(
+        text_to_translate
+    )
     for language in data:
         formatLang.append(language.to_dict())
     return render_template(
         "index.html",
         languages=formatLang,
-        text_to_translate="O que deseja traduzir?",
-        translate_from="pt",
-        translate_to="en",
-        translated="What do you want to translate?"
+        text_to_translate=text_to_translate,
+        translate_from=translate_from,
+        translate_to=translate_to,
+        translated=translated
         )
 
 
